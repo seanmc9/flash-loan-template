520c8673cb2cc436a2f3d4b47d78ee8ac54908e2
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-03-22 18:15:44 -0400

    chore: add remapping pt 2

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,58 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-import {Test, console} from "@forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import "@erc7399/IERC7399.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+/// @dev Basic flash loan borrower. It allows to examine the state of the borrower during the callback.
+/// credit: https://github.com/alcueca/erc7399-wrappers/blob/main/test/MockBorrower.sol
+contract BasicBorrower {
+    using SafeERC20 for IERC20;
 
-    function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+    bytes32 public constant ERC3156PP_CALLBACK_SUCCESS = keccak256("ERC3156PP_CALLBACK_SUCCESS");
+    IERC7399 lender;
+    LoanReceiver loanReceiver;
+
+    uint256 public flashBalance;
+    address public flashInitiator;
+    address public flashAsset;
+    uint256 public flashAmount;
+    uint256 public flashFee;
+
+    constructor(IERC7399 lender_) {
+        setLender(lender_);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function setLender(IERC7399 lender_) public {
+        lender = lender_;
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    /// @dev Flash loan callback
+    function onFlashLoan(
+        address initiator,
+        address paymentReceiver,
+        address asset,
+        uint256 amount,
+        uint256 fee,
+        bytes calldata
+    )
+        external
+        returns (bytes memory)
+    {
+        require(msg.sender == address(lender), "MockBorrower: Untrusted lender");
+        require(initiator == address(this), "MockBorrower: External loan initiator");
+
+        flashInitiator = initiator;
+        flashAsset = asset;
+        flashAmount = amount;
+        flashFee = fee;
+        loanReceiver.retrieve(asset);
+        flashBalance = IERC20(asset).balanceOf(address(this));
+        IERC20(asset).safeTransfer(paymentReceiver, amount + fee);
+
+        return abi.encode(ERC3156PP_CALLBACK_SUCCESS);
+    }
+
+    function flashBorrow(address asset, uint256 amount) public returns (bytes memory) {
+        return lender.flash(address(loanReceiver), asset, amount, "", this.onFlashLoan);
     }
 }
