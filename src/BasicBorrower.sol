a8200d3297706986211016706f9e468f79a691c6
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-03-22 21:43:21 -0400

    chore: add retrival funcs and Ownable

--- src/BasicBorrower.sol
+++ src/BasicBorrower.sol
@@ -2,15 +2,16 @@
 pragma solidity ^0.8.19;
 
 import "@erc7399/IERC7399.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IWETH9.sol";
-import "@forge-std/console.sol";
 
 import {IERC20Metadata as IERC20} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /// @dev Basic flash loan borrower. It allows to examine the state of the borrower during the callback.
 /// credit: https://github.com/alcueca/erc7399-wrappers/blob/main/test/MockBorrower.sol
-contract BasicBorrower {
+contract BasicBorrower is Ownable {
     using SafeERC20 for IERC20;
 
     bytes32 public constant ERC3156PP_CALLBACK_SUCCESS = keccak256("ERC3156PP_CALLBACK_SUCCESS");
@@ -25,6 +26,14 @@ contract BasicBorrower {
 
     function setLender(IERC7399 lender_) public {
         lender = lender_;
+    }
+
+    function retrieveNativeAsset() public onlyOwner {
+        Address.sendValue(payable(msg.sender), address(this).balance);
+    }
+
+    function retrieveERC20(address asset) public onlyOwner {
+        IERC20(asset).safeTransfer(msg.sender, IERC20(asset).balanceOf(address(this)));
     }
 
     /// @dev Flash loan callback
@@ -47,7 +56,7 @@ contract BasicBorrower {
         return abi.encode(ERC3156PP_CALLBACK_SUCCESS);
     }
 
-    function flashBorrow(address asset, uint256 amount) public payable returns (bytes memory) {
+    function flashBorrow(address asset, uint256 amount) public payable onlyOwner returns (bytes memory) {
         return lender.flash(address(this), asset, amount, "", this.onFlashLoan);
     }
 }
